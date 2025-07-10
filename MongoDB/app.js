9227ac7fed976ffeb0dd34261c10d968cc38d1d1
Jerome <nietsemorej@gmail.com>
2025-07-10 14:06:13 -0700

    Add initial MongoDB setup with connection script, package files, and sample data

---
+++ MongoDB/app.js
@@ -0,0 +1,14 @@
+const { MongoClient } = require('mongodb');
+const url = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(url, { serverSelectionTimeoutMS: 5000 });
+
+console.log("Connecting to MongoDB...");
+
+client.connect()
+  .then(() => {
+    console.log("Database created!");
+    return client.close();
+  })
+  .catch(err => {
+    console.error("Error connecting to MongoDB", err);
+  });
