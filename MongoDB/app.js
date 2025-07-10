7874b2a2ff5487f3e0874a8a48aba961a8ebdf5b
Jerome <nietsemorej@gmail.com>
2025-07-10 14:12:58 -0700

    Refactor MongoDB connection script to create a collection instead of closing the connection immediately. Added error handling for collection creation.

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -6,8 +6,15 @@ console.log("Connecting to MongoDB...");
 
 client.connect()
   .then(() => {
-    console.log("Database created!");
-    return client.close();
+    // console.log("Database created!");
+    // return client.close();
+    var dbo = client.db("nodemongo");
+    dbo.createCollection("customers").then(res => {
+      console.log("Collection created!");
+      client.close();
+    }).catch(err => {
+      console.error("Error creating collection", err);
+    });
   })
   .catch(err => {
     console.error("Error connecting to MongoDB", err);
